38f8bdb40b4640baa1a7780e7a3c0db4d60a9767
grace-harrison <47007208+grace-harrison@users.noreply.github.com>
2019-03-07 00:21:04 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  lab-1.5-gharrison15
+//
+//  Created by Grace M. Harrison on 3/6/19.
+//  Copyright © 2019 Grace M. Harrison. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Test\n");
+    return 0;
+}
